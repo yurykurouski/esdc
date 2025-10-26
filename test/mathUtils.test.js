be1c5c4aaf64bd33d0b83a50525325fa05da49a9
Yury Kurouski <ymorpheus@gmail.com>
2025-10-26 19:34:00 +0100

    test: improve formatting and add additional test cases for utility functions

--- test/mathUtils.test.js
+++ test/mathUtils.test.js
@@ -2,25 +2,25 @@ import { expect } from 'chai';
 import { add, subtract, multiply, divide } from '../utils/mathUtils.js';
 
 describe('mathUtils', () => {
-    it('adds two numbers', () => {
-        expect(add(2, 3)).to.equal(5);
+  it('adds two numbers', () => {
+    expect(add(2, 3)).to.equal(5);
+  });
+
+  it('subtracts the second number from the first', () => {
+    expect(subtract(10, 4)).to.equal(6);
+  });
+
+  it('multiplies two numbers', () => {
+    expect(multiply(3, 7)).to.equal(21);
+  });
+
+  describe('divide', () => {
+    it('divides the first number by the second', () => {
+      expect(divide(10, 2)).to.equal(5);
     });
 
-    it('subtracts the second number from the first', () => {
-        expect(subtract(10, 4)).to.equal(6);
+    it('throws when dividing by zero', () => {
+      expect(() => divide(10, 0)).to.throw('Cannot divide by zero');
     });
-
-    it('multiplies two numbers', () => {
-        expect(multiply(3, 7)).to.equal(21);
-    });
-
-    describe('divide', () => {
-        it('divides the first number by the second', () => {
-            expect(divide(10, 2)).to.equal(5);
-        });
-
-        it('throws when dividing by zero', () => {
-            expect(() => divide(10, 0)).to.throw('Cannot divide by zero');
-        });
-    });
+  });
 });
--- test/stringUtils.test.js
+++ test/stringUtils.test.js
@@ -2,37 +2,37 @@ import { expect } from 'chai';
 import { capitalize, reverseString, isPalindrome } from '../utils/stringUtils.js';
 
 describe('stringUtils', () => {
-    describe('capitalize', () => {
-        it('capitalizes the first letter of a string', () => {
-            expect(capitalize('hello')).to.equal('Hello');
-        });
-
-        it('throws when input is not a string', () => {
-            expect(() => capitalize(123)).to.throw('Input must be a string');
-        });
+  describe('capitalize', () => {
+    it('capitalizes the first letter of a string', () => {
+      expect(capitalize('hello')).to.equal('Hello');
     });
 
-    describe('reverseString', () => {
-        it('reverses the provided string', () => {
-            expect(reverseString('abc')).to.equal('cba');
-        });
+    it('throws when input is not a string', () => {
+      expect(() => capitalize(123)).to.throw('Input must be a string');
+    });
+  });
 
-        it('throws when input is not a string', () => {
-            expect(() => reverseString({})).to.throw('Input must be a string');
-        });
+  describe('reverseString', () => {
+    it('reverses the provided string', () => {
+      expect(reverseString('abc')).to.equal('cba');
     });
 
-    describe('isPalindrome', () => {
-        it('returns true for palindromes', () => {
-            expect(isPalindrome('level')).to.be.true;
-        });
-
-        it('returns false for non-palindromes', () => {
-            expect(isPalindrome('test')).to.be.false;
-        });
-
-        it('throws when input is not a string', () => {
-            expect(() => isPalindrome([])).to.throw('Input must be a string');
-        });
+    it('throws when input is not a string', () => {
+      expect(() => reverseString({})).to.throw('Input must be a string');
     });
+  });
+
+  describe('isPalindrome', () => {
+    it('returns true for palindromes', () => {
+      expect(isPalindrome('level')).to.be.true;
+    });
+
+    it('returns false for non-palindromes', () => {
+      expect(isPalindrome('test')).to.be.false;
+    });
+
+    it('throws when input is not a string', () => {
+      expect(() => isPalindrome([])).to.throw('Input must be a string');
+    });
+  });
 });
